9d6bb8f8396686cbb09cd794156dc8d558a98eab
renatoas77 <renato.antonio7@hotmail.com>
2022-01-27 22:03:06 -0300

    A impressao do tabuleiro foi melhorada

--- Program.cs
+++ Program.cs
@@ -8,11 +8,19 @@ namespace Xadrez
     {
         static void Main(string[] args)
         {
-            PosicaoXadrez pos = new PosicaoXadrez('c', 7);
+            Tabuleiro tab = new Tabuleiro(8,8);
+            tab.ColocaPeca(new Torre(tab,Cor.Preta),new Posicao(3,4));
+            tab.ColocaPeca(new Torre(tab,Cor.Preta),new Posicao(4,3));
+            tab.ColocaPeca(new Rei(tab,Cor.Preta),new Posicao(2,7));
+            tab.ColocaPeca(new Torre(tab, Cor.Branca), new Posicao(7, 2));
+            tab.ColocaPeca(new Rei(tab, Cor.Branca), new Posicao(5, 5));
 
-            Console.WriteLine(pos);
 
-            Console.WriteLine(pos.toPosicao());
+
+
+            Tela.ImprimirTabuleiro(tab);
+
+
         }
     }
 }
--- Tela.cs
+++ Tela.cs
@@ -8,19 +8,38 @@ class Tela
     {
         for (int i = 0; i < tab.Linhas; i++)
         {
+            Console.Write(8 - i);
             for (int j = 0; j < tab.Colunas; j++)
             {
+
                 if (tab.peca(i, j) == null)
                 {
                     Console.Write(" -");
                 }
                 else
                 {
-                    Console.Write(" " + tab.peca(i, j));
+                    Console.Write(" ");
+                    Tela.ImprimirPeca(tab.peca(i, j));
                 }
 
             }
             Console.WriteLine();
+        }
+        Console.WriteLine("  a b c d e f g h ");
+    }
+
+    public static void ImprimirPeca(Peca peca)
+    {
+        if (peca.Cor == Cor.Branca)
+        {
+            Console.Write(peca);
+        }
+        else
+        {
+            ConsoleColor aux = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write(peca);
+            Console.ForegroundColor = aux;
         }
     }
 }
